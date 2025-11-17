aa4695602589cd25ab43779e46ccc7624f92f3f8
Jeff1010-web <60737736+Jeff1010-web@users.noreply.github.com>
2025-11-17 14:56:18 +0900

    reset Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Company5000 from './components/Company/Company5000';
+import HeroHeader5000 from './components/Header/Header5000';
+import Hero5000 from './components/Hero/Hero5000';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Company5000/>
+      <HeroHeader5000 />
+      <Hero5000 />
     </div>
   );
 }
