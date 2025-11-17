27257353b6002a313628d44d81932ae227f9bd01
Jeff1010-web <tech51030@gmail.com>
2025-11-17 11:58:04 +0900

    fix: push empty

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Voice5000 from './components/Voice/Voice5000';
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Voice5000/>
+      <Voice5000/> 
     </div>
   );
 }
