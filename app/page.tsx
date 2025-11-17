19fa76bf5cf9900d5b7931cc46d840a5ebaed31c
Jeff1010-web <tech51030@gmail.com>
2025-11-17 15:30:02 +0900

    feat: complete hero5005

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import HeroHeader5000 from './components/Header/Header5000';
-import Hero5000 from './components/Hero/Hero5000';
+import Header5005 from './components/Header/Header5005';
+import Hero5005 from './components/Hero/Hero5005';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <HeroHeader5000 />
-      <Hero5000 />
+      <Header5005/>
+      <Hero5005/>
     </div>
   );
 }
