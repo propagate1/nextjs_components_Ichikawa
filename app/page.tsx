e357d8c489834601f83cce8aaa221eb06d2b5ee3
Jeff1010-web <tech51030@gmail.com>
2025-11-21 09:14:18 +0900

    feat: complete Voice5000

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Product5001 from './components/Products/Product5001';
+import Voice5001 from './components/Voice/Voice5001';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Product5001/>
+      <Voice5001/>
     </div>
   );
 }
