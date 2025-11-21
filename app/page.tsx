f438f7f332bba5d4196e9ae884b73cf53ca21d58
Jeff1010-web <tech51030@gmail.com>
2025-11-21 09:56:17 +0900

    feat: complete Features5001

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Product5001 from './components/Products/Product5001';
+import Features5001 from './components/Features/Features5001';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Product5001/>
+      <Features5001/>
     </div>
   );
 }
