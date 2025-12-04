5c35d26a88549f377fa00926cda382555f10dd5a
Jeff1010-web <tech51030@gmail.com>
2025-12-04 11:07:04 +0900

    feat: complete About5004

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Service5003 from './components/Service/Service5003';
+import About5004 from './components/About/About5004';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Service5003/>
+      <About5004/>
     </div>
   );
 }
