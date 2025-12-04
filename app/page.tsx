14ec1c8cd60811cb7c901e6bf5eeda86cc15a0c8
Jeff1010-web <tech51030@gmail.com>
2025-12-04 11:44:38 +0900

    feat: complete Hero5014

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Service5003 from './components/Service/Service5003';
+import Hero5014 from './components/Hero/Hero5014';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Service5003/>
+      <Hero5014/>
     </div>
   );
 }
