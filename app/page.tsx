af9558953f0b1afd4161630ca642cdecfdc99f20
Jeff1010-web <tech51030@gmail.com>
2025-12-04 10:01:49 +0900

    feat: complete Achievement5002

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Service5003 from './components/Service/Service5003';
+import Achievement5002 from './components/Achievement/Achievement5002';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Service5003/>
+      <Achievement5002/>
     </div>
   );
 }
