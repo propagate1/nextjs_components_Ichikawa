361833308b947b3e4c9e55fd89df8c4a3e6eba1f
Jeff1010-web <tech51030@gmail.com>
2026-01-14 17:55:36 +0900

    fix: fixed Challenges5002 highlighting

--- app/components/Challenges/Challenges5002.tsx
+++ app/components/Challenges/Challenges5002.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { desc } from 'framer-motion/client';
 import Image from 'next/image';
 
 export default function Challenges5002() {
@@ -8,7 +9,9 @@ export default function Challenges5002() {
       number: '1',
       title: '肩こり・頭痛',
       description:
-        'やわらかい枕、高さがあっていない枕、睡眠中に形が変わる枕は頭が安定せず、首を一晩中いじめてしまいます。睡眠中に首を休めるためには、理想的な首の角度にして頭をしっかり支える必要があります。',
+        'やわらかい枕、高さがあっていない枕、睡眠中に形が変わる枕は頭が安定せず、首を一晩中いじめてしまいます。',
+      description1:
+      '睡眠中に首を休めるためには、理想的な首の角度にして頭をしっかり支える必要があります。',
       image: '/img/Challenges5002_Bg_1.PNG',
       imagePosition: 'left' as const,
     },
@@ -17,7 +20,9 @@ export default function Challenges5002() {
       number: '2',
       title: '腰痛',
       description:
-        '睡眠中の姿勢が悪かったり、寝返りがうまくうてないと腰痛の原因になります。適正な枕を作り、正しい睡眠姿勢にすることで寝返りがうちやすくなります。',
+        '睡眠中の姿勢が悪かったり、寝返りがうまくうてないと腰痛の原因になります。',
+      description1:
+      '適正な枕を作り、正しい睡眠姿勢にすることで寝返りがうちやすくなります。',
       image: '/img/Challenges5002_Bg_2.PNG',
       imagePosition: 'right' as const,
     },
@@ -26,8 +31,10 @@ export default function Challenges5002() {
       number: '3',
       title: '不眠症',
       description:
-        '枕の高さが適正でないと脊椎が緊張し、副交感神経が優位になりません。そのため寝つきが悪くなり、夜中に目が覚めやすくなります。適正な枕を使うことでリラックスでき、スッキリ目覚めることができます。',
+        '枕の高さが適正でないと脊椎が緊張し、副交感神経が優位になりません。そのため寝つきが悪くなり、夜中に目が覚めやすくなります。',
       image: '/img/Challenges5002_Bg_3.PNG',
+      description1:
+      '適正な枕を使うことでリラックスでき、スッキリ目覚めることができます。',
       imagePosition: 'left' as const,
     },
     {
@@ -35,14 +42,16 @@ export default function Challenges5002() {
       number: '4',
       title: 'いびき・無呼吸',
       description:
-        '睡眠中に気道(空気の通り道)が狭くなるといびきをかきます。枕が高すぎても低すぎてもいびきや睡眠時無呼吸症候群の原因になります。気道を狭めないためには適正な高さの枕が必要です。',
+        '睡眠中に気道(空気の通り道)が狭くなるといびきをかきます。枕が高すぎても低すぎてもいびきや睡眠時無呼吸症候群の原因になります。',
+      description1:
+      '気道を狭めないためには適正な高さの枕が必要です。',
       image: '/img/Challenges5002_Bg_4.PNG',
       imagePosition: 'right' as const,
     },
   ];
 
   return (
-    <div className="w-full bg-[linear-gradient(to_right,#e5e7eb_1px,transparent_1px),linear-gradient(to_bottom,#e5e7eb_1px,transparent_1px)]  bg-[size:10px_10px] py-12 md:py-16 lg:py-20">
+    <div className="w-full bg-[linear-gradient(to_right,#e5e7eb_1px,transparent_1px),linear-gradient(to_bottom,#e5e7eb_1px,transparent_1px)]  bg-[size:10px_10px] py-12 md:py-16 lg:py-20 font-['Yu_Mincho','Hiragino_Mincho_ProN','MS_PMincho',serif]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12">
 
         {/* Symptoms List */}
@@ -69,14 +78,15 @@ export default function Challenges5002() {
               {/* Text Content Section */}
               <div className="w-full lg:w-1/2 flex flex-col justify-center">
                 {/* Heading */}
-                <h2 className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold text-[#595326] mb-4 md:mb-6 leading-tight">
-                  <span className=' border-2'>症</span><span className=' border-2'>状</span><span className=' text-center border-2 border-[#595326] text-red-600'> {symptom.number}</span> <span className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>{symptom.title}</span>
+                <h2 className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold text-[#484944] mb-4 md:mb-6 leading-tight">
+                  <span className=' border-2'>症</span><span className=' border-2'>状</span><span className=' text-center border-2 border-[#484944] text-red-600'> {symptom.number}</span> <span className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>{symptom.title}</span>
                 </h2>
                 <div className=' border-t-2 border-dotted border-amber-800 mb-2'>
                 </div>
                 {/* Description */}
-                <p className="text-base md:text-lg lg:text-xl text-black leading-relaxed">
-                  {symptom.description}
+                <p className="text-base md:text-lg lg:text-xl text-[#484944] leading-relaxed">
+                  <span>{symptom.description}</span>
+                  <span className=' bg-[#fbeca9]'>{symptom.description1}</span>
                 </p>
               </div>
             </div>
